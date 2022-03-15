0e15bec407cf4c3d3a98d68438c37c4744ee83f5
Nicolas Dupont <nicolas@akeneo.com>
2022-03-15 18:00:04 +0100

    Complete Manipulate CSV examples

--- src/Cli/ReadCsvFileLeagueWayCommand.php
+++ src/Cli/ReadCsvFileLeagueWayCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Cli;
 
 use League\Csv\Reader;
+use League\Csv\Statement;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,18 +21,82 @@ class ReadCsvFileLeagueWayCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $path = 'data/movies-100.csv';
-        $csv = Reader::createFromPath($path, 'r');
-        $csv->setHeaderOffset(0); // use the first line as header for rows
 
-        $header = $csv->getHeader();
-        var_dump($header);
+        // read all lines
+        if (false) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0); // use the first line as header for rows
+            $header = $csv->getHeader();
+            var_dump($header);
+            $rows = $csv->getRecords();
+            foreach ($rows as $row) {
+                var_dump($row);
+            }
 
-        $rows = $csv->getRecords();
-        foreach ($rows as $row) {
-            var_dump($row);
+            $output->writeln("I read the CSV File ".$path);
         }
 
-        $output->writeln("I read the CSV File ".$path);
+        // count
+        if (false) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0); // use the first line as header for rows
+            var_dump($csv->count());
+        }
+
+        // headers
+        if (false) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0); // use the first line as header for rows
+            $headers = $csv->getHeader();
+            var_dump($headers);
+        }
+
+        // fetch a specific rows by its index
+        if (false) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0); // use the first line as header for rows
+            var_dump($csv->fetchOne(10));
+        }
+
+        // fetch specific rows by its index
+        if (false) {
+            // fetch 3 rows starting from the 10th row
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0);
+            $stmt = Statement::create()
+                ->offset(10)
+                ->limit(3);
+            $records = $stmt->process($csv);
+            foreach ($records as $row) {
+                var_dump($row);
+            }
+        }
+
+        // filter rows
+        if (false) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0);
+            $stmt = Statement::create()
+                ->where(function($row) { return strpos($row['title'], 'Deadpool') !== false; });
+            $records = $stmt->process($csv);
+            foreach ($records as $row) {
+                var_dump($row);
+            }
+        }
+
+        // sort rows
+        if (true) {
+            $csv = Reader::createFromPath($path, 'r');
+            $csv->setHeaderOffset(0);
+            $stmt = Statement::create()
+                ->orderBy(function($row1, $row2) { return strcmp($row1['title'], $row2['title']); })
+                ->limit(5);
+            $records = $stmt->process($csv);
+            foreach ($records as $row) {
+                var_dump($row);
+            }
+        }
+
 
         return 0;
     }
--- src/Cli/WriteCsvFileLeagueWayCommand.php
+++ src/Cli/WriteCsvFileLeagueWayCommand.php
@@ -19,18 +19,29 @@ class WriteCsvFileLeagueWayCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $rows = [
-            ['id', 'title', 'poster', 'overview', 'release_date', 'genres'],
-            [181808, "Star Wars: The Last Jedi", "https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg", "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.", 1513123200, "Documentary"],
-            [383498, "Deadpool 2", "https://image.tmdb.org/t/p/w500/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 1526346000, "Action, Comedy, Adventure"],
-            [157336, "Interstellar", "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg", "Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",1415145600,"Adventure, Drama, Science Fiction"]
+        // insert / erase the file
+        if (false) {
+            $rows = [
+                ['id', 'title', 'poster', 'overview', 'release_date', 'genres'],
+                [181808, "Star Wars: The Last Jedi", "https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg", "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.", 1513123200, "Documentary"],
+                [383498, "Deadpool 2", "https://image.tmdb.org/t/p/w500/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 1526346000, "Action, Comedy, Adventure"],
+                [157336, "Interstellar", "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg", "Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",1415145600,"Adventure, Drama, Science Fiction"]
+            ];
+
+            $path = 'data/new-file2.csv';
+            $csv = Writer::createFromPath($path, 'w');
+            $csv->insertAll($rows);
+            $output->writeln("I wrote the CSV File ".$path);
+        }
+
+        $appendRows = [
+            [324857, "Spider-Man: Into the Spider-Verse", "https://image.tmdb.org/t/p/w500/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg", "Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson 'Kingpin' Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.",1544140800,"Action, Adventure, Animation, Science Fiction, Comedy"],
+            [456740, "Hellboy", "https://image.tmdb.org/t/p/w500/bk8LyaMqUtaQ9hUShuvFznQYQKR.jpg", "Hellboy comes to England, where he must defeat Nimue, Merlin's consort and the Blood Queen. But their battle will bring about the end of the world, a fate he desperately tries to turn away.",1554944400,"Fantasy, Action"]
         ];
-
         $path = 'data/new-file2.csv';
-        $csv = Writer::createFromPath($path, 'w');
-        $csv->insertAll($rows);
-
-        $output->writeln("I wrote the CSV File ".$path);
+        $csv = Writer::createFromPath($path, 'a+');
+        $csv->insertAll($appendRows);
+        $output->writeln("I append lines in the CSV File ".$path);
 
         return 0;
     }
