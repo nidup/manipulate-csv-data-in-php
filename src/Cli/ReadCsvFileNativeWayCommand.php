8525a793ccf623f890f0f3fe01154169fa810b96
Nicolas Dupont <nicolas@akeneo.com>
2022-03-18 19:44:34 +0100

    Add CSV to JSON conversion example

--- src/Cli/ReadCsvFileNativeWayCommand.php
+++ src/Cli/ReadCsvFileNativeWayCommand.php
@@ -19,12 +19,31 @@ class ReadCsvFileNativeWayCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $path = 'data/movies-100.csv';
-        if (($handle = fopen($path, "r")) !== false) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-                var_dump($data);
+        // read the file
+        if (false) {
+            $handle = fopen($path, "r");
+            while (($row = fgetcsv($handle)) !== false) {
+                var_dump($row);
             }
             fclose($handle);
         }
+
+        // combine rows into an associative array
+        // read the rows
+        $rows = [];
+        $handle = fopen($path, "r");
+        while (($row = fgetcsv($handle)) !== false) {
+            $rows[] = $row;
+        }
+        fclose($handle);
+        // Remove the first one that contains headers
+        $headers = array_shift($rows);
+        // Combine the headers with each following row
+        $array = [];
+        foreach ($rows as $row) {
+            $array[] = array_combine($headers, $row);
+        }
+        var_dump($array);
 
         $output->writeln("I read the CSV File ".$path);
 
